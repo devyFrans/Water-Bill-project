9b8a13e7840db892d45386121e6e1784fd463f62
Francis Niog <francisniog205@gmail.com>
2026-05-01 15:20:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const users = [
+    {
+        serialID: 505501200365,
+        name: "Francis Niog",
+        address: "Sta.Maria, Bulacan",
+    },
+
+    {
+        serialID: 12345678912,
+        name: "Joshua Ruiz",
+        address: "North Caloocan, QC",
+    },
+
+    {
+        serialID: 987560127418,
+        name: "Raffy Amosco",
+        address: "Sapang Palay, SJDM",
+    }
+];
+
+
+const navbar = document.querySelector('#navbar');
+const logoutBtn = document.querySelector('#logoutBtn');
+const loginSection = document.querySelector('#loginSection');
+const serialID = document.querySelector('#serialID');
+const serialID_error = document.querySelector('#serialID_error');
+const searchBtn = document.querySelector('#searchBtn');
+const statusError = document.querySelector('#statusError');
+const dashboardSection = document.querySelector('#dashboardSection');
+const userInfoTable =  document.querySelector('#userInfoTable');
+
+serialID.addEventListener('click', function(){
+    
+});
